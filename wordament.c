19d3ed114b0f8406da804040b829cd548f58be81
Tom <25043847+Douile@users.noreply.github.com>
2019-02-22 17:53:13 +0000

    v0.3
    Calculates points per word and sorts (estimate)

--- wordament.c
+++ wordament.c
@@ -3,14 +3,24 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#pragma semicolon 1
+
 // switch to lower case because word list is lower case
 #define ALPHABET_SIZE 26
 #define OFFSET 97
-#define WORDLIST "./words/words_3_9.txt"
+// #define WORDLIST "./words/words_3_9.txt"
 #define MAXLEN 20
 #define NEWLINE '\n'
 #define ROW 4
+#define POSITIONS ROW*ROW
+const unsigned short POINTS[ALPHABET_SIZE] = {2,5,3,3,1,5,4,4,2,10,6,3,4,2,2,4,10,2,2,2,4,6,6,9,5,8};
 typedef enum {false, true} bool;
+
+// argv globals
+bool DEBUG = false;
+bool VERBOOSE = false;
+char * WORDLIST = "./words/words_3_9.txt";
+int MIN_LENGTH = 0;
 
 static void catch_handler(int signo) {
   if (signo == SIGINT) {
@@ -22,23 +32,73 @@ static void catch_handler(int signo) {
   }
 }
 
+void parse_args(int argc,char **argv) {
+  char *wordlist = NULL;
+  for (int i=1;i<argc;i++) {
+    char *arg = argv[i];
+    if (strcmp(arg,"-d") == 0 || strcmp(arg,"--debug") == 0) {
+      DEBUG = true;
+      printf("Debug mode enabled...\n");
+    } else if (strcmp(arg,"-v") == 0 || strcmp(arg,"--verboose") == 0) {
+      VERBOOSE = true;
+      printf("Verboose mode enabled...\n");
+    } else if (strcmp(arg,"-m") == 0 || strcmp(arg,"--min-len") == 0) {
+      if (i+1 < argc) {
+        i++;
+        MIN_LENGTH = atoi(argv[i]);
+        printf("Min length set to %d\n",MIN_LENGTH);
+      }
+    } else if (strcmp(arg,"-h") == 0 || strcmp(arg,"--help") == 0) {
+      printf("Usage: %s [-h] [-v] [-d] [-m n] ...wordlist\n\n  -h, --help = Print this help message\n  -v, --verboose = Run in verboose mode\n  -d, --debug = Run in debug mode\n  -m [n],--min-len [n] = set the min length for words to be found to [n]\n  wordlist = file to load wordlist from\n\n",argv[0]);
+      exit(EXIT_SUCCESS);
+    } else {
+      wordlist = arg;
+    }
+  }
+  if (wordlist != NULL) {
+    free(WORDLIST);
+    WORDLIST = wordlist;
+  }
+}
+
 typedef struct Trie_t Trie_t;
 typedef struct Trie_t {
   bool end;
   char letter;
   Trie_t *children[ALPHABET_SIZE];
 };
+typedef struct {
+  char *word;
+  float points;
+} Word_t;
 typedef struct Wordlist_t Wordlist_t;
 typedef struct Wordlist_t {
-  char *word;
+  //char *word;
+  Word_t *word;
   Wordlist_t *next;
 };
-typedef struct {
-  char *chars;
-  int size;
-} Charstack_t;
 
 const int CHILDREN_SIZE = sizeof(Trie_t *)*ALPHABET_SIZE;
+
+char * float_string(float n) {
+  char *text = malloc(sizeof(char)*10);
+  sprintf(text,"%F",n);
+  int len = strlen(text);
+  for (int i=1;i<=len;i++) {
+    int x = len-i;
+    if (text[x] == '0') {
+      text[x] = 0;
+    } else if (text[x] == '.') {
+      text[x] = 0;
+      break;
+    } else {
+      break;
+    }
+  }
+  int n_len = strlen(text)+1;
+  text = realloc(text,sizeof(char)*n_len);
+  return text;
+}
 
 Trie_t * new_trie(char letter,bool end) {
   // doesn't work but did before when not in function
@@ -164,8 +224,7 @@ char * get_pos(int maxlen) {
 
 void print_board(char **board) {
   int maxlen=0;
-  int size = ROW*ROW;
-  for (int i=0;i<size;i++) {
+  for (int i=0;i<POSITIONS;i++) {
     int l = strlen(board[i]);
     if (l > maxlen) {
       maxlen = l;
@@ -215,101 +274,212 @@ bool has_visited(unsigned int *v,int x,int y) {
   return visited;
 }
 
-Wordlist_t * new_wordlist(char *word) {
+// Wordlist_t * new_wordlist(char *word) {
+//   Wordlist_t *wordlist = NULL;
+//   wordlist = malloc(sizeof(Wordlist_t));
+//   wordlist->word = word;
+//   wordlist->next = NULL;
+//   return wordlist;
+// }
+//
+// void add_word(Wordlist_t *head,char *word) {
+//   Wordlist_t *new_word = new_wordlist(word);
+//   Wordlist_t *node = head;
+//   while (node->next != NULL) {
+//     node = node->next;
+//     if (strcmp(node->word,word) == 0) {
+//       return;
+//     }
+//   }
+//   node->next = new_word;
+// }
+
+Word_t * new_word(char *word,float points) {
+  Word_t * node = malloc(sizeof(Word_t));
+  node->word = word;
+  node->points = points;
+  return node;
+}
+
+Wordlist_t * new_wordlist(Word_t *word) {
   Wordlist_t *wordlist = NULL;
   wordlist = malloc(sizeof(Wordlist_t));
   wordlist->word = word;
   wordlist->next = NULL;
   return wordlist;
 }
-
-void add_word(Wordlist_t *head,char *word) {
+void add_word(Wordlist_t *head,Word_t *word) {
   Wordlist_t *new_word = new_wordlist(word);
   Wordlist_t *node = head;
+  // 2 loops inefficient but only using 1 loop wasn't working
   while (node->next != NULL) {
     node = node->next;
-    if (strcmp(node->word,word) == 0) {
+    if (strcmp(word->word,node->word->word) == 0 && word->points == node->word->points) {
+      if (VERBOOSE == true || DEBUG == true) printf("Duplicate word %s\n",word->word);
+      free(word->word);
+      free(word);
+      free(new_word);
       return;
     }
   }
-  node->next = new_word;
+  node = head;
+  while (node->next != NULL) {
+    if (word->points <= node->next->word->points) break;
+    node = node->next;
+  }
+  if (node->next == NULL) {
+    node->next = new_word;
+    if (DEBUG == true) printf("Appended word %s\n",new_word->word->word);
+  } else {
+    new_word->next = node->next;;
+    node->next = new_word;
+    if (DEBUG == true) printf("Inserted word %s\n",new_word->word->word);
+  }
 }
 
-// Wordlist_t * new_wordlist() {
-//   Wordlist_t *words = malloc(sizeof(Wordlist_t));
-//   words->size = 0;
-//   return words;
-// }
-//
-// void add_word(Wordlist_t *words,char *word) {
-//   int size = words->size + 1;
-//   words->size = size;
-//   words = realloc(words,sizeof(Wordlist_t)+sizeof(char*)*(size+1));
-//   words->words[size] = word;
-//   printf("New word:%s\n",word);
-// }
-//
-// Charstack_t * new_charstack() {
-//   Charstack_t *stack = malloc(sizeof(Charstack_t)+sizeof(char));
-//   stack->size = 1;
-//   stack->chars[0] = 0;
-//   return stack;
-// }
-// void add_char(Charstack_t *stack,char c) {
-//   int size = stack->size+1;
-//   stack->size = size;
-//   stack = realloc(stack,sizeof(Charstack_t)+sizeof(char)*size);
-//   stack->chars[size-1] = c;
-//   stack->chars[size] = 0;
-//   printf("%s\n",stack->chars);
-// }
-//
-// void reduce_char(Charstack_t *stack) {
-//   int size = stack->size-1;
-//   stack->size = size;
-//   //stack->chars[size+1] = NULL;
-//   stack->chars[size] = 0;
-//   stack = realloc(stack,sizeof(Charstack_t)+sizeof(char)*size);
-// }
-void add_char(char *stack,char c) {
-  size_t size = strlen(stack);
-  //stack = realloc(stack,sizeof(char)*(size+1));
-  stack[size] = c;
-  stack[size+1] = 0;
+int calc_points(const char *pos) {
+  int len = strlen(pos);
+  int points = 0;
+  for (int i=0;i<len;i++) {
+    char c = pos[i];
+    if (c == '/') {
+      return 20;
+    } else if (c == '-') {
+      return 12;
+    } else {
+      points += POINTS[c-OFFSET];
+    }
+  }
+  points *= len; // may be incorrect
+  return points;
 }
 
-void reduce_char(char *stack) {
-  size_t size = strlen(stack);
-  if (size > 0) {
-    //stack = realloc(stack,sizeof(char)*(size-1));
+// void add_char(char *stack,char c) {
+//   size_t size = strlen(stack);
+//   //stack = realloc(stack,sizeof(char)*(size+1));
+//   stack[size] = c;
+//   stack[size+1] = 0;
+// }
+//
+// void reduce_char(char *stack) {
+//   size_t size = strlen(stack);
+//   if (size > 0) {
+//     //stack = realloc(stack,sizeof(char)*(size-1));
+//     stack[size-1] = 0;
+//   }
+// }
+int stack_size(unsigned int *stack) {
+  int i = 0;
+  while (i <= POSITIONS+1 && stack[i] != 0) {
+    i++;
+  }
+  return i;
+}
+
+unsigned int stack_get(unsigned int *stack,int n) {
+  unsigned int v = stack[n];
+  if (v > 0) {
+    return v-1;
+  }
+  return 0;
+}
+
+void stack_add(unsigned int *stack,int x,int y) {
+  unsigned short pos = y*ROW+x+1;
+  int size = stack_size(stack);
+  if (size < POSITIONS+1) {
+    stack[size] = pos;
+    stack[size+1] = 0;
+  }
+}
+
+void stack_reduce(unsigned int *stack) {
+  int size = stack_size(stack);
+  if (size > 1) {
     stack[size-1] = 0;
   }
 }
 
-void find_words(Trie_t *node,char **board,Wordlist_t *wordlist,char *stack,unsigned int *visited,int ox,int oy) {
-  if (has_visited(visited,ox,oy) == true) return;
+bool stack_includes(unsigned int *stack,int x,int y) {
+  unsigned int pos = y*ROW+x+1;
+  for (int i=0;i<stack_size(stack);i++) {
+    if (pos == stack[i]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+void debug_string(const char *chars) {
+  int i = 0;
+  while (i<20) {
+    char c = chars[i];
+    i++;
+    printf("%c:%d,",c,c);
+    if (c == 0) {
+      break;
+    }
+  }
+  printf("  <-- Length: %d\n",i);
+}
+
+void find_words(Trie_t *node,char **board,int *point_board,Wordlist_t *wordlist,unsigned int *stack,unsigned int *visited,int ox,int oy) {
+  //if (has_visited(visited,ox,oy) == true) return;
+  if (stack_includes(stack,ox,oy) == true) return;
   char *c_string = board[ROW*oy+ox];
-  for (int i=0;i<strlen(c_string);i++) {
+  int len = strlen(c_string);
+  for (int i=0;i<len;i++) {
     char c = c_string[i];
     // add handling for x/y, -start and -end
     if (node->children[c-OFFSET] != NULL) {
       node = node->children[c-OFFSET];
-      add_char(stack,c);
+      // add_char(stack,c);
       //printf("Built: %s\n",stack);
     } else {
-      for (int j=0;j<i;j++) {
-        reduce_char(stack);
-      }
+      // for (int j=0;j<i;j++) {
+      //   reduce_char(stack);
+      // }
       return;
     }
   }
-  set_visited(visited,ox,oy,true);
+  stack_add(stack,ox,oy);
+  //set_visited(visited,ox,oy,true);
   if (node->end == true) {
-    size_t size = strlen(stack);
-    char *word = malloc(sizeof(char)*size);
-    strcpy(word,stack);
-    //printf("%s\n",word);
-    add_word(wordlist,word);
+    int size = stack_size(stack);
+    int char_count = 1;
+    float points = 0.0;
+    for (int i=0;i<size;i++) {
+      unsigned int pos = stack_get(stack,i);
+      //printf("%d:\n",pos);
+      //debug_string(board[pos]);
+      char_count += strlen(board[pos]);
+      points += (float) point_board[pos];
+    }
+    //printf("Complete\n");
+    if (char_count >= 8) {
+      points *= 2.5;
+    } else if (char_count >= 6) {
+      points *= 2;
+    } else if (char_count >= 5) {
+      points *= 1.5;
+    }
+    char *word_s = malloc(sizeof(char)*char_count);
+    int x = 0;
+    for (int i=0;i<size;i++) {
+      char *pos = board[stack_get(stack,i)];
+      for (int j=0;j<strlen(pos);j++) {
+        word_s[x] = pos[j];
+        x++;
+      }
+    }
+    word_s[x] = 0;
+    if (x > MIN_LENGTH) {
+      if (VERBOOSE == true || DEBUG == true) printf("Found word: %s\n",word_s);
+      Word_t *word = new_word(word_s,points);
+      add_word(wordlist,word);
+    } else {
+      free(word_s);
+    }
   }
   for (int dx=-1;dx<=1;dx++) {
     int x = ox+dx;
@@ -318,12 +488,12 @@ void find_words(Trie_t *node,char **board,Wordlist_t *wordlist,char *stack,unsig
       int y = oy+dy;
       if (y < 0 || y >= ROW) continue;
       if (dy == 0 && dx == 0) continue;
-      find_words(node,board,wordlist,stack,visited,x,y);
+      find_words(node,board,point_board,wordlist,stack,visited,x,y);
     }
   }
-  set_visited(visited,ox,oy,false);
+  //set_visited(visited,ox,oy,false);
   for (int i=0;i<strlen(c_string);i++) {
-    reduce_char(stack);
+    stack_reduce(stack);
   }
 }
 
@@ -331,36 +501,12 @@ void find_words(Trie_t *node,char **board,Wordlist_t *wordlist,char *stack,unsig
 int main(int argc,char **argv) {
   signal(SIGINT,catch_handler);
   signal(SIGSEGV,catch_handler);
-  char* wordlist = NULL;
-  bool debug = false;
-  bool verboose = false;
-  int minlen = 0;
-  for (int i=1;i<argc;i++) {
-    char *arg = argv[i];
-    if (strcmp(arg,"-d") == 0 || strcmp(arg,"--debug") == 0) {
-      debug = true;
-    } else if (strcmp(arg,"-v") == 0 || strcmp(arg,"--verboose") == 0) {
-      verboose = true;
-    } else if (strcmp(arg,"-m") == 0 || strcmp(arg,"--min-len") == 0) {
-      if (i+1 < argc) {
-        i++;
-        minlen = atoi(argv[i]);
-      }
-    } else if (strcmp(arg,"-h") == 0 || strcmp(arg,"--help") == 0) {
-      printf("Usage: %s [-h] [-v] [-d] [-m n] ...wordlist\n\n  -h, --help = Print this help message\n  -v, --verboose = Run in verboose mode\n  -d, --debug = Run in debug mode\n  -m [n],--min-len [n] = set the min length for words to be found to [n]\n  wordlist = file to load wordlist from\n\n",argv[0]);
-      return 0;
-    } else {
-      wordlist = arg;
-    }
-  }
-  if (wordlist == NULL) {
-    wordlist = WORDLIST;
-  }
-  printf("Loading wordlist: %s\n",wordlist);
-  FILE *file = fopen(wordlist,"r");
+  parse_args(argc,argv);
+  printf("Loading wordlist: %s\n",WORDLIST);
+  FILE *file = fopen(WORDLIST,"r");
   Trie_t *head = parse_wordlist(file);
   fclose(file);
-  if (debug == true) {
+  if (DEBUG == true) {
     bool ass = head->children['a'-OFFSET]->children['s'-OFFSET]->children['s'-OFFSET]->end;
     if (ass == true) {
       printf("Ass\n");
@@ -370,44 +516,54 @@ int main(int argc,char **argv) {
   }
   printf("Loading done\n");
   //  print_trie(head,0,4);
-  size_t size = sizeof(char*)*ROW*ROW;
-  if (verboose == true || debug == true) {
+  size_t size = sizeof(char*)*POSITIONS;
+  if (VERBOOSE == true || DEBUG == true) {
     printf("Board size: %d\n",size);
   }
   char **board = malloc(size);
+  int *point_board = malloc(sizeof(int)*POSITIONS);
   for (int y=0;y<ROW;y++) {
     for (int x=0;x<ROW;x++) {
       printf("Pos (%d,%d): ",x+1,y+1);
       char *pos = get_pos(10);
       int loc = (y*ROW)+x;
       board[loc] = pos;
+      point_board[loc] = calc_points(pos);
+    }
+  }
+  if (DEBUG == true) {
+    for (int y=0;y<ROW;y++) {
+      for (int x=0;x<ROW;x++) {
+        int pos = y*ROW+x;
+        printf("Checking (%d,%d)\n",x,y);
+        int size = strlen(board[pos]);
+        char *copy = malloc(sizeof(char)*(size+1));
+        strcpy(copy,board[pos]);
+        printf("%s %d - %d\n",copy,size,point_board[pos]);
+        free(copy);
+      }
     }
   }
   print_board(board);
-  Wordlist_t *w_head = new_wordlist("");
+  Wordlist_t *w_head = new_wordlist(NULL);
   for (int y=0;y<ROW;y++) {
     for (int x=0;x<ROW;x++) {
       unsigned int *visited = malloc(sizeof(unsigned int));
       *visited = 0;
       //Charstack_t *stack = new_charstack();
-      char stack[ROW*ROW];
+      unsigned int stack[POSITIONS+1];
       stack[0] = 0;
-      find_words(head,board,w_head,stack,visited,x,y);
+      find_words(head,board,point_board,w_head,stack,visited,x,y);
     }
   }
   printf("Words:\n");
   Wordlist_t *node = w_head->next;
   unsigned int w_count = 0;
   while (node != NULL) {
-    if (minlen > 0) {
-      if (strlen(node->word) >= minlen) {
-        printf("%s\n",node->word);
-        w_count++;
-      }
-    } else {
-      printf("%s\n",node->word);
-      w_count++;
-    }
+    char *f = float_string(node->word->points);
+    printf("%s - %s\n",f,node->word->word);
+    free(f);
+    w_count++;
     node = node->next;
   }
   printf("Found %d words\n",w_count);
